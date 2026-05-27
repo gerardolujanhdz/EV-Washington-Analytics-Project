9811f8e78fdaf32d38714e8921075dd863880cfa
gerardolujanhdz <gerardolujanhdz.gmail.com>
2026-05-27 13:21:04 -0500

    Updated 05_indexes.sql: Creating indexes for foreign keys to speed up joins for both tables

--- sql/05_indexes.sql
+++ sql/05_indexes.sql
@@ -2,12 +2,24 @@
 -- Creating indexes for foreign keys to speed up joins 
 --
 
-CREATE INDEX idx_ev_location_id 
-  ON ev_washington(location_id);
+-- Population table indexes
+CREATE INDEX idx_population_location_id 
+  ON population(location_id);
 
-CREATE INDEX idx_ev_coordinate_id 
-  ON ev_washington(coordinate_id);
+CREATE INDEX idx_population_coordinate_id 
+  ON population(coordinate_id);
 
+CREATE INDEX idx_population_vehicle_id 
+  ON population(vehicle_id);
 
+-- Registration table indexes
+CREATE INDEX idx_registration_location_id 
+  ON registration(location_id);
+
+CREATE INDEX idx_registration_vehicle_id 
+  ON registration(vehicle_id);
+
+CREATE INDEX idx_registration_compliance_id 
+  ON registration(compliance_id);
 
 
