71b36e6489bd44ddb035c20c624acc6bbb6b092b
gerardolujanhdz <gerardolujanhdz.gmail.com>
2026-05-27 13:46:37 -0500

    Updated 06_exploration_queries.sql: Some basic data exploration queries for both the registration and population tables

--- sql/06_exploration_queries.sql
+++ sql/06_exploration_queries.sql
@@ -1,45 +1,93 @@
+
 -- Basic Exploration Queries
+
+-- Population Table
 
 -- Most Common EV Make sorted 
 SELECT 
-  make,
+  v.make,
   COUNT(make) AS total
-FROM ev_washington
+FROM population
+LEFT JOIN vehicles v ON population.vehicle_id = v.vehicle_id 
 GROUP BY make
-ORDER BY count DESC;
+ORDER BY total DESC;
 
 -- Most Common EV model sorted 
 SELECT 
-  make, 
-  model,
+  v.make, 
+  v.model,
   COUNT(*) as count 
-FROM ev_washington
+FROM population
+LEFT JOIN vehicles v ON population.vehicle_id = v.vehicle_id
 GROUP BY 
-  make,
-  model 
+  v.make,
+  v.model 
 ORDER BY count DESC;
 
 -- Most EVs by county sorted 
 SELECT 
-  county,
+  l.county,
   COUNT(county) as count 
-FROM ev_washington
+FROM population
+LEFT JOIN locations l on population.location_id = l.location_id
 GROUP BY county 
 ORDER BY count DESC;
 
 -- Most EVs by city sorted 
 SELECT 
-  city,
+  l.city,
   COUNT(city) as count 
-FROM ev_washington
+FROM population
+LEFT JOIN locations l ON population.location_id = l.location_id
 GROUP BY city 
 ORDER BY count DESC;
 
 -- EV type breakdown
 SELECT 
-  ev_type,
+  v.ev_type,
   COUNT(ev_type) AS total 
-FROM ev_washington
+FROM population
+LEFT JOIN vehicles v ON population.vehicle_id = v.vehicle_id
 GROUP BY ev_type;
 
+-- Most common makes of EV per County
+SELECT 
+  l.county,
+  v.make,
+  COUNT(*) as count
+FROM population
+  LEFT JOIN locations l ON population.location_id = l.location_id
+  LEFT JOIN vehicles v ON population.vehicle_id = v.vehicle_id
+GROUP BY
+  l.county,
+  v.make
+ORDER BY count DESC
+
+-- Registration Table Queries 
+--
+
+-- Number of registrations by year 
+SELECT 
+ transaction_year,
+ COUNT(transaction_year) as count 
+FROM registration
+GROUP BY transaction_year
+ORDER BY count DESC;
+
+-- Registration Transaction Type and Count 
+SELECT 
+  transaction_type,
+  COUNT(transaction_type) as count 
+FROM registration 
+GROUP BY transaction_type
+ORDER BY count DESC;
+
+-- Registration by County
+SELECT 
+  l.county,
+  COUNT(*) AS count 
+FROM registration
+LEFT JOIN locations l ON registration.location_id = l.location_id 
+GROUP BY county
+ORDER BY count DESC;
 
