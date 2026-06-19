95e15b2108113cb928e43ffe2cf6fbdea0d1f31f
gerardolujanhdz <gerardolujanhdz.gmail.com>
2026-06-19 14:17:58 -0500

    adding geometric median plot fn

--- python_scripts/visualizations.py
+++ python_scripts/visualizations.py
@@ -11,12 +11,54 @@ import plotly.graph_objects as go
 from urllib.request import urlopen
 import json
 
-from geometric_median_computation import sql_query, load_in_coordinates
+from geometric_median_computation import load_in_coordinates, main as gm_main
 from county_population_imports import main as cpi_main
 
 # constants
 LOG_PATH = os.path.join(os.path.dirname(__file__), "../logs/visualizations.log")
 DB_PATH = os.path.join(os.path.dirname(__file__), "../database/ev_washington.db")
+
+region_dict = {
+    "San Juan": "Northwest",
+    "Kitsap": "Peninsulas",
+    "Clallam": "Peninsulas",
+    "Jefferson": "Peninsulas",
+    "Grays Harbor": "Peninsulas",
+    "Mason": "Peninsulas",
+    "Pacific": "Southwest",
+    "Thurston": "Metro Puget Sound",
+    "Lewis": "Southwest",
+    "Wahkiakum": "Southwest",
+    "Cowlitz": "Southwest",
+    "Clark": "Southwest",
+    "Skamania": "Southwest",
+    "Pierce": "Metro Puget Sound",
+    "King": "Metro Puget Sound",
+    "Snohomish": "Metro Puget Sound",
+    "Skagit": "Northwest",
+    "Whatcom": "Northwest",
+    "Okanogan": "North Central",
+    "Chelan": "North Central",
+    "Douglas": "North Central",
+    "Kittitas": "North Central",
+    "Yakima": "Wine Country",
+    "Klickitat": "Wine Country",
+    "Grant": "North Central",
+    "Benton": "Wine Country",
+    "Ferry": "Eastern",
+    "Stevens": "Eastern",
+    "Pend Oreille": "Eastern",
+    "Lincoln": "Eastern",
+    "Spokane": "Eastern",
+    "Adams": "Eastern",
+    "Franklin": "Wine Country",
+    "Walla Walla": "Wine Country",
+    "Columbia": "Wine Country",
+    "Garfield": "Wine Country",
+    "Asotin": "Wine Country",
+    "Whitman": "Eastern",
+    "Island": "Northwest",
+}
 
 # configuring/creating a logger
 logging.basicConfig(
@@ -90,6 +132,61 @@ def add_log_count(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def dataframe_transformations(
+    df: pd.DataFrame, county_fips_dict: dict[str, str], pop_counts: dict[str, int]
+) -> pd.DataFrame:
+    df = add_df_fips_index(county_fips_dict, df)
+    df = county_grouping_df(df)
+    df = add_pop_count(pop_counts, df)
+    df = add_log_count(df)
+    df = add_per_person(df)
+    return df
+
+
+def gm_plot(
+    df: pd.DataFrame, gm_dict: dict[str, list[float]], counties: dict, region_dict: dict
+) -> go.Figure:
+
+    # choroplethmapbox underlay (color coded counties by region), scattermapbox over lay with corresponding gm approximation
+    df = df[
+        ["County", "fips_location"]
+    ].copy()  # left with county, fips_location, need a region column
+    df["Region"] = df["County"].map(region_dict)
+
+    # creating pd.dataframe from dictonary
+    region_gm_df = pd.DataFrame(
+        [(key, value[0], value[1]) for key, value in gm_dict.items()],
+        columns=["region", "longitude", "latitude"],
+    )
+
+    fig = px.choropleth_map(
+        data_frame=df,
+        geojson=counties,
+        locations="fips_location",
+        color="Region",
+        featureidkey="properties.COUNTY",
+        center={"lat": 47.45, "lon": -120.9},
+        zoom=6,
+        hover_name="County",
+        hover_data={"fips_location": False},
+    )
+
+    fig.add_trace(
+        go.Scattermap(
+            lat=region_gm_df["latitude"],
+            lon=region_gm_df["longitude"],
+            text=region_gm_df["region"],
+            mode="markers",
+            marker=dict(size=12, color="rgb(0, 0,0)"),
+            hovertemplate="<b>%{text}</b><br>Lat: %{lat:.3f}<br>Lon: %{lon:.3f}<extra></extra>",
+            name="Geometric Median",
+        )
+    )
+
+    print(df)
+    return fig
+
+
 def plot_choropleth_map_raw(df: pd.DataFrame, counties: dict) -> go.Figure:
     fig = px.choropleth_map(
         data_frame=df,
@@ -144,17 +241,6 @@ def plot_choropleth_map_norm(df: pd.DataFrame, counties: dict) -> go.Figure:
     return fig
 
 
-def dataframe_transformations(
-    df: pd.DataFrame, county_fips_dict: dict[str, str], pop_counts: dict[str, int]
-) -> pd.DataFrame:
-    df = add_df_fips_index(county_fips_dict, df)
-    df = county_grouping_df(df)
-    df = add_pop_count(pop_counts, df)
-    df = add_log_count(df)
-    df = add_per_person(df)
-    return df
-
-
 def main() -> None:
     try:
         with sqlite3.connect(DB_PATH) as connection:
@@ -163,15 +249,19 @@ def main() -> None:
 
         pop_counts = cpi_main()
         counties = load_counties()
+        gm_dict = gm_main()
         county_fips_dict = get_county_fips_dict(counties)
         coordinates_df = dataframe_transformations(
             coordinates_df, county_fips_dict, pop_counts
         )
-        plot_choropleth_map_raw(coordinates_df, counties).show()
-        plot_choropleth_map_norm(coordinates_df, counties).show()
+        # plot_choropleth_map_raw(coordinates_df, counties).show()
+        # plot_choropleth_map_norm(coordinates_df, counties).show()
+        gm_plot(coordinates_df, gm_dict, counties, region_dict).show()
     except Exception as e:
         logger.error("Error: %s", e)
+        raise
 
 
 if __name__ == "__main__":
     main()
+
