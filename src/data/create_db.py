28410b961709e08fbb92134a7625db8c6efb2ace
gerardolujanhdz <gerardolujanhdz.gmail.com>
2026-07-01 20:29:40 -0500

    fixing file paths

---
+++ src/data/create_db.py
@@ -0,0 +1,38 @@
+import sqlite3
+import os
+import logging
+
+# constants
+DB_PATH = os.path.join(
+    os.path.dirname(__file__), "../../data/database/ev_washington.db"
+)
+LOG_PATH = os.path.join(os.path.dirname(__file__), "../../logs/create_db.log")
+FILENAME = "ev_washington.db"
+
+# configuring/creating a logger
+logging.basicConfig(
+    filename=LOG_PATH,
+    encoding="utf-8",
+    level=logging.DEBUG,
+    filemode="w",
+    format="%(levelname)s:%(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    try:
+        if not os.path.isfile(DB_PATH):
+            with sqlite3.connect(DB_PATH) as connection:
+                logger.info("Database %s formed", FILENAME)
+                print(f"Database {FILENAME} formed")
+        else:
+            print("Database already exists")
+            logger.info("Database already exists")
+    except Exception as e:
+        print(f"Database {FILENAME} not formed, error : {e}")
+        logger.error("Error: %s", e)
+
+
+if __name__ == "__main__":
+    main()
